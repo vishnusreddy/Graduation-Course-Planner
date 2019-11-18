ea71f750eec67354e82cef06bb8d642308ba4fc7
Vishnu S Reddy <52468309+Vishnu-S-Reddy@users.noreply.github.com>
2019-11-18 19:55:06 +0530

    close but not yet

--- temp2.c
+++ temp2.c
@@ -1,5 +1,6 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+int time=0;
 struct node
 {
     int vertex;
@@ -10,12 +11,14 @@ struct Graph
 {
     int numVertices;
     int* visited;
+    int* depart;
     struct node** adjLists; // we need int** to store a two dimensional array. Similary, we need struct node** to store an array of Linked lists
 };
 struct Graph* createGraph(int);
 void addEdge(struct Graph*, int, int);
 void printGraph(struct Graph*);
-void DFS(struct Graph*, int);
+void DFS(struct Graph*, int, int);
+void doTopologicalSort(struct Graph*,int );
 int main()
 {
     struct Graph* graph = createGraph(4);
@@ -24,26 +27,47 @@ int main()
     addEdge(graph, 1, 2);
     addEdge(graph, 2, 3);
     
+    doTopologicalSort(graph,4);
     printGraph(graph);
-    DFS(graph, 2);
     
     return 0;
 }
-void DFS(struct Graph* graph, int vertex) {
+void DFS(struct Graph* graph, int vertex, int time) {
         struct node* adjList = graph->adjLists[vertex];
         struct node* temp = adjList;
         
         graph->visited[vertex] = 1;
-        printf("Visited %d \n", vertex);
+        time++;
     
         while(temp!=NULL) {
             int connectedVertex = temp->vertex;
         
             if(graph->visited[connectedVertex] == 0) {
-                DFS(graph, connectedVertex);
+                DFS(graph, connectedVertex,time);
             }
             temp = temp->next;
-        }       
+        }
+        graph->depart[(time)]=vertex;
+        time++;       
+}
+void doTopologicalSort(struct Graph* graph, int n)
+{
+    
+    for(int i=0;i<n;i++)
+    {
+        if(!graph->visited[i])
+        {
+            DFS(graph, i, time);
+        }
+    }
+    //Now printing the order in which they are visited
+    for(int i = 2*n-1; i>=0;i--)
+    {
+        if(graph->depart[i]!=0)
+        {
+            printf("%d ",graph->depart[i]);
+        }
+    }
 }
  
 struct node* createNode(int v)
@@ -57,11 +81,9 @@ struct Graph* createGraph(int vertices)
 {
     struct Graph* graph = malloc(sizeof(struct Graph));
     graph->numVertices = vertices;
- 
     graph->adjLists = malloc(vertices * sizeof(struct node*));
-    
     graph->visited = malloc(vertices * sizeof(int));
- 
+    graph->depart=calloc(2*vertices,sizeof(int));
     int i;
     for (i = 0; i < vertices; i++) {
         graph->adjLists[i] = NULL;
