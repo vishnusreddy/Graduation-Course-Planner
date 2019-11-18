68e8e99fd9cccc7b2945775ab3312ad69d444cce
Vishnu S Reddy <52468309+Vishnu-S-Reddy@users.noreply.github.com>
2019-11-18 18:01:11 +0530

    Working without using DFS

--- temp1.c
+++ temp1.c
@@ -1,121 +1,145 @@
-#include <stdio.h>
-#include <stdlib.h>//include "graph.h"
-//adjacency list element
-struct graph_node {
-    struct graph_node *link;
-    int v;
-};
-typedef struct graph_node graph_node;
-struct graph {
-    int n;   // # of vertices
-    int edgeCount;  // # of edges
-    graph_node **alist;
-};
-typedef struct graph Graph;
+#include<stdio.h>
+#include<stdlib.h>
+ 
+#define MAX 100
+ 
+void create_graph();
+void add(int vertex);
+int del();
+int isEmpty();
+int find_indegree_of_vertex(int vertex);
+ 
+int total_vertices;
+int adjacent_matrix[MAX][MAX];
+int queue[MAX];
+int front = -1;
+int rear = -1;
 
-Graph *graphCreate(int n)
-{
-    Graph *g = malloc(sizeof(Graph));
-    g->n = n;
-    g->edgeCount = 0;
-    g->alist = calloc(n, sizeof(graph_node *));
-    return g;
-}
-
-void graphAddEdge(Graph *g, int u, int v)
-{
-    graph_node *e = malloc(sizeof(graph_node));
-    e->v = v;
-    e->link = g->alist[u];
-    g->alist[u] = e;
-    g->edgeCount++;
-}
-
-void readgraph(Graph *g)
-{   int i,j;
-    while(1)
-    {
-        printf("Enter the source and the destination vertex..");
-        scanf("%d%d",&i,&j);
-        if((i==-1)&&(j==-1))
-           break;
-        graphAddEdge(g,i,j);
-    }
-
-}
-
-void graphDestroy(Graph *g)
-{
-    for(int u = 0; u < g->n; u++) {
-        while(g->alist[u]) {
-            graph_node *nextnode = g->alist[u]->link;
-            free(g->alist[u]);
-            g->alist[u] = nextnode;
-        }
-    }
-    free(g->alist);
-    free(g);
-}
-
-int graphSize(const Graph *g)
-{
-    return g->n;
-}
-
-int graphEdgeCount(const Graph *g)
-{
-    return g->edgeCount;
-}
-
-void dfs(Graph *g,int v,int *visited, int *departed,int time)
-{
-    graph_node *p;
-    time++;
-    int w;
-    visited[v]=1;
-    printf("%d ",v);
-   
-    for(p=(g->alist[v]);p!=NULL;p=(p->link))
-    {
-      w=p->v;
-      if(visited[w]==0)//not visited
-        dfs(g,w,visited,departed,time);
-    }
-    departed[time]=v;
-    time++;
-}
-
-void doTopologicalSort(Graph *g,int *visited, int *departed)
-{
-    int time=0;
-    for(int i=0;i<g->n;i++)
-    {
-        if(!visited[i])
-        {
-            dfs(g,i,visited,departed,&time);
-        }
-    }
-    for(int i= (2*(g->n)-1);i>=0;i--)
-        if(departed[i]!=0)
-            printf("%d ",departed[i]);
-
-}
-//////////////////////////////////////////////////////////////////////
 int main()
 {
-   int i,v,k,n,src;    
-   printf("Enter the number of vertices..");
-   scanf("%d",&n);
-   Graph *g = graphCreate(n);
-   readgraph(g);
-   int *visited;
-   int *departed;
-   visited=calloc(sizeof(int),n);
-   departed=calloc(sizeof(int),n);
-   doTopologicalSort(g,visited,departed);
-   printf("The vertices reachable from %d using DFS are..\n",src);
-   
-   for(i=0;i<n;i++)
-       visited[i]=0;
-  
+      int i, vertex, count, topological_sort[MAX], indegree[MAX];
+      create_graph();
+      for(i = 0; i < total_vertices; i++)
+      {
+            indegree[i] = find_indegree_of_vertex(i);
+            if(indegree[i] == 0)
+            {
+                  add(i);
+            }
+      }
+      count = 0;
+      while(!isEmpty() && count < total_vertices)
+      {
+            vertex = del();
+            topological_sort[++count] = vertex;
+            for(i = 0; i < total_vertices; i++)
+            {
+                  if(adjacent_matrix[vertex][i] == 1)
+                  {
+                        adjacent_matrix[vertex][i] = 0;
+                        indegree[i] = indegree[i] - 1;
+                        if(indegree[i] == 0)
+                        {
+                              add(i);
+                        }
+                  }
+            }
+      }
+      if(count < total_vertices)
+      {
+            printf("Graph is Cyclic. Therefore, Topological Ordering Not Possible\n");
+            exit(1);
+      }
+      printf("Topological Order of Vertices\n");
+      for(i = 1; i <= count; i++)
+      {
+            printf("%3d", topological_sort[i]);
+      }
+      printf("\n");
+      return 0;
+}
+ 
+void add(int vertex)
+{
+      if(rear == MAX - 1)
+      {
+            printf("Queue Overflow\n");
+      }
+      else
+      {
+            if(front == -1) 
+            {
+                  front = 0;
+            }
+            rear = rear + 1;
+            queue[rear] = vertex ;
+      }
+}
+ 
+int isEmpty()
+{
+      if(front == -1 || front > rear)
+      {
+            return 1;
+      }
+      else
+      {
+            return 0;
+      }
+}
+ 
+int del()
+{
+      int element;
+      if(front == -1 || front > rear)
+      {
+            printf("Queue Underflow\n");
+            exit(1);
+      }
+      else
+      {
+            element = queue[front];
+            front = front + 1;
+            return element;
+      }
+}
+ 
+int find_indegree_of_vertex(int vertex)
+{
+      int count, total_indegree = 0;
+      for(count = 0; count < total_vertices; count++)
+      {
+            if(adjacent_matrix[count][vertex] == 1)
+            {
+                  total_indegree++;
+            }
+      }
+      return total_indegree;
+}
+ 
+void create_graph()
+{
+      int count, maximum_edges, origin_vertex, destination_vertex;
+      printf("Enter number of vertices:\t");
+      scanf("%d", &total_vertices);
+      maximum_edges = total_vertices * (total_vertices - 1);
+      for(count = 1; count <= maximum_edges; count++)
+      {
+            printf("Enter Edge [%d] co-ordinates (-1 -1 to quit)\n", count);
+            printf("Enter Origin Vertex:\t");
+            scanf("%d", &origin_vertex);
+            printf("Enter Destination Vertex:\t");
+            scanf("%d", &destination_vertex);
+            if((origin_vertex == -1) && (destination_vertex == -1))
+            {
+                  break;
+            }
+            if(origin_vertex >= total_vertices || destination_vertex >= total_vertices || origin_vertex < 0 || destination_vertex < 0)
+            {
+                  printf("Edge Co-ordinates are Invalid\n");
+                  count--;
+            }
+            else
+                  adjacent_matrix[origin_vertex][destination_vertex] = 1;
+      }
 }
